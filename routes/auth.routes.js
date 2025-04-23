6b5c6c729d670c971f5371a287c265b7a0d6af87
Kd526 <davisthoyah@gmail.com>
2025-04-23 20:31:58 +0300

    updates

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,7 @@
 import {Router} from 'express'
 //authenticate user login
 const authRouter = Router()
-
+///authentication routes    
 authRouter.post('/sign-up', (req, res) => res.send({title: 'Sign up'}))
 authRouter.post('/sign-in', (req, res) => res.send({title: 'Sign in'}))
 authRouter.post('/sign-out', (req, res) => res.send({title: 'Sign out'}))
